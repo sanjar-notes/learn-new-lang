36a060a3a566c1adfb932f8c534aea1fb80d1af8
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-16 13:03:00 +0530

    Remove zim files

--- 2._Things_to_learn/1._Fundamentals_1/3._Medium_1_-_Understanding_basics/block_scope.py
+++
@@ -1,7 +0,0 @@
-if True:
-    x = 2
-# is x accessble here?
-
-while some_true_condition:
-    x = 2
-# is x accessible here?
--- 2._Things_to_learn/1._Fundamentals_1/3._Medium_1_-_Understanding_basics/function_scope.py
+++
@@ -1,8 +0,0 @@
-def f():
-	# some code
-    x  = 23
-	def g():
-		# g's code
-        x = 25
-		pass
-    # has x changed
